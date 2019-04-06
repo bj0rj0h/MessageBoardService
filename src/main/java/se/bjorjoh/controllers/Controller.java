7af6d7c2993868d4ef2bfb5d8c133b4161a9bbd6
Björn Johanson <bjorjoh.dev@gmail.com>
2019-04-06 10:43:23 +0200

    added connection to service layer

--- src/main/java/se/bjorjoh/controllers/Controller.java
+++ src/main/java/se/bjorjoh/controllers/Controller.java
@@ -1,31 +1,46 @@
 package se.bjorjoh.controllers;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import se.bjorjoh.models.Message;
+import se.bjorjoh.services.BoardService;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class Controller {
 
-    @RequestMapping(value = "/messages",method = RequestMethod.GET)
+    @Autowired
+    private BoardService boardService;
+
+    @RequestMapping(value = "/messages",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Message> getMessages(HttpServletResponse response) {
         response.setStatus(HttpServletResponse.SC_OK);
         return new ArrayList<Message>();
     }
 
-    @RequestMapping(value = "/messages",method = RequestMethod.POST)
-    public Message addMessage(HttpServletResponse response) {
+    @RequestMapping(value = "/messages",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Message addMessage(@Valid @RequestBody Message message,
+                              HttpServletResponse response) {
+
+        boardService.addMessage(message);
         response.setStatus(HttpServletResponse.SC_CREATED);
         return new Message();
     }
 
-    @RequestMapping(value = "/messages/{messageId}",method = RequestMethod.PUT)
+    @RequestMapping(value = "/messages/{messageId}",
+            method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public Message editMessage(@PathVariable("messageId") String messageId,HttpServletResponse response) {
         response.setStatus(HttpServletResponse.SC_OK);
         return new Message();
